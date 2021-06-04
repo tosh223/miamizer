57e1f48e75a3f9d0e2a3236a3200d0d9767fec6a
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-06-04 23:25:26 +0900

    add option --split

--- miamizer.rb
+++ miamizer.rb
@@ -21,14 +21,14 @@ module Miamizer
       resources.push(%(--target "#{resource.physical_resource_id}"))
     end
 
-    cmd = "bundle exec miam -e -o IAMfile --no-progress #{resources.join(' ')}"
+    cmd = "bundle exec miam -e --split --no-progress #{resources.join(' ')}"
     run_cmd!(cmd)
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   if ARGV[0].nil?
-    puts 'Please set a stack_name.'
+    puts 'Please set a stack-name.'
     exit(1)
   else
     status = Miamizer.export(ARGV[0])
